8f57e0efe88e7b61028c3149cdeee9b9be8c74a9
Guglielmo Cola <guglielmocola@gmail.com>
2022-01-21 11:35:22 +0100

    New setup and requirements files.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
+
 setup(
     name='retweetcascade',
+    description='Estimation of cascade retweet trees.',
+    version='0.1.0',
     packages=[
         "retweetcascade",
     ],
-    version='0.1.0',
-    description='Estimation of cascade retweet trees.',
+    install_requires=[
+        'pandas>=1.2.4',
+        'pandasticsearch>=0.5.3',
+    ],
     author='Guglielmo Cola',
+    author_email='guglielmo.cola@iit.cnr.it',
     license='MIT',
+    url='https://github.com/guglielmocola/RetweetCascade'
 )
